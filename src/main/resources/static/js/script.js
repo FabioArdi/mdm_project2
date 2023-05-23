9007fb3fc370a669989a05e48e83040073566c41
FabioArdi <arditfab@students.zhaw.ch>
2023-05-23 15:38:32 +0200

    Added frontend

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,7 +2,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('styleTransferForm');
     const fileInput = document.getElementById('fileInput');
     const artistSelect = document.getElementById('artistSelect');
-    const outputImage = document.getElementById('outputImage');
+    const outputContainer = document.getElementById('outputContainer');
+    const outputImage = outputContainer.querySelector('#outputImage');
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -10,18 +11,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         formData.append('file', fileInput.files[0]);
         formData.append('artist', artistSelect.value);
 
+        const button = event.target.querySelector('button[type="submit"]');
+        const spinner = button.querySelector('.spinner-border');
+        button.disabled = true; // Disable the button during the request
+        spinner.classList.remove('d-none'); // Show the loading spinner
+
         fetch('/styletransfer', {
             method: 'POST',
             body: formData
         })
         .then(response => response.text())
         .then(imagePath => {
-            const imageUrl = imagePath
+            const imageUrl = imagePath;
             outputImage.innerHTML = `<img src="${imageUrl}" alt="Transformed Image">`;
         })
         .catch(error => {
             console.error('Error:', error);
             outputImage.innerHTML = 'An error occurred.';
+        })
+        .finally(() => {
+            button.disabled = false; // Enable the button after the response is received
+            spinner.classList.add('d-none'); // Hide the loading spinner
         });
     });
+
 });
